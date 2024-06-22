6534ecb8b8255f3eea44369a7d59561c6bde2148
yagnesh767 <131937564+yagnesh767@users.noreply.github.com>
2024-06-22 10:34:07 +0530

    Create Power of 2.cpp

---
+++ Power of 2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int n,s,p,q;
+	cout<<" Base = ";
+	cin>>n;
+	cout<<" Exponent = ";
+	cin>>s;
+	p = pow(n,s);
+	cout<<" Powered values "<<p<<endl;
+}
